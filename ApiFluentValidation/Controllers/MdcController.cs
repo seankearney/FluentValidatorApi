c8f5695d07fcd3c59fec8f220c7a74d096e0cb06
Sean Kearney <git@seankearney.com>
2020-10-29 15:53:36 -0400

    initial

---
+++ ApiFluentValidation/Controllers/MdcController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFluentValidation.Controllers
+{
+    /// <summary>
+    /// A command for adding user file to the system
+    /// </summary>
+    public class AddFileCommand
+    {
+        /// <summary>
+        /// Device id the file is added from
+        /// </summary>
+        public Guid DeviceId { get; set; }
+    }
+
+    public class AddFileCommandValidator : AbstractValidator<AddFileCommand>
+    {
+        /// <summary>
+        /// Creates instance of the <see cref="AddFileCommandValidator"/>
+        /// </summary>
+        public AddFileCommandValidator()
+        {
+            RuleFor(m => m.DeviceId)
+                .NotEmpty()
+                .WithErrorCode("GetError(25002)");
+        }
+    }
+
+    [Route("[controller]")]
+    public class MdcController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(@"POST to /mdc with `{""DeviceId"":""xxxxx""}");
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromBody]AddFileCommand addFileCommand)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok("VALID!");
+            }
+            else
+            {
+                return BadRequest(ModelState.Values);
+            }
+        }
+    }
+}
